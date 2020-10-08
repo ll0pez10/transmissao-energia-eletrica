2d431a3cd9b496a26e76d37aecccf4c56764b04d
lorenzolucas <72564054+lorenzolucas@users.noreply.github.com>
2020-10-08 17:45:59 +0200

    Update metodos_linhas.py

--- metodos_linhas.py
+++ metodos_linhas.py
@@ -1,11 +1,12 @@
 # função para calcular o raio equivalente
+
 def raio_eq(n, rext, R):
     re = (rext*n*(R**(n-1)))**(n)
     return re
 
 # potencia caracteristica
 
-
+#a
 def Pnat(Vs, Vr, Ir):
     Zc = Vr/Ir
     Pnat = (Vs**2)/Zc.real

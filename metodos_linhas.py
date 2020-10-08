28714f3a3a0fbcf4b4b6b8e5562bfd23c68f1a34
lorenzolucas <72564054+lorenzolucas@users.noreply.github.com>
2020-10-08 18:53:27 +0200

    Update metodos_linhas.py

--- metodos_linhas.py
+++ metodos_linhas.py
@@ -1,5 +1,11 @@
 # função para calcular o raio equivalente
 def raio_eq(n, rext, R):
+
+    #R = raio do cicrulo que sera o novo condutor
+    #rext = raio do feixe de condutor
+    re = (rext*n*(R**(n-1)))**(n)
+    return re
+
     re = (rext*n*(R**(n-1)))**(n)
     return re
 

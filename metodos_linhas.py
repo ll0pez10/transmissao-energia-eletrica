4f219fe7b86bc085b598825b96c43461ceb80a2d
lorenzolucas <72564054+lorenzolucas@users.noreply.github.com>
2020-10-08 19:40:36 +0200

    Update metodos_linhas.py

--- metodos_linhas.py
+++ metodos_linhas.py
@@ -1,17 +1,12 @@
 # função para calcular o raio equivalente
 def raio_eq(n, rext, R):
-
     #R = raio do cicrulo que sera o novo condutor
     #rext = raio do feixe de condutor
-    re = (rext*n*(R**(n-1)))**(n)
-    return re
-
-    re = (rext*n*(R**(n-1)))**(n)
+    re = sqrt((rext*n*(R**(n-1))))
     return re
 
 # potencia caracteristica
 
-#aaalorenzo
 
 def Pnat(Vs, Vr, Ir):
     Zc = Vr/Ir

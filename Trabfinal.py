27f4d3f0a9e1c664defccd0567378d6067b06340
igorruys <igorruys@gmail.com>
2020-11-11 23:29:08 -0300

    Adicionando a compensacao

--- Trabfinal.py
+++ Trabfinal.py
@@ -40,109 +40,7 @@ CondutoresPos = {"Bluejay": (raio_eq(4, CondutoresEspecs["Bluejay"][1], .475*sqr
                                  (23.2, 33.2, 23.2, 23.2, 33.2, 23.2),
                                  (-8.8, 42.7, 8.8, 42.7), 4)
                  }
-                 
-#Posicoes X e Y de cada condutor individual (includindo pararaios) para o Bluejay. Os valores
-#foram calculados no arquivo do Mathematica fornecido pelo professor
-XcondBluejay = [-15.5141, -16.1859, -16.1859, -15.5141, 0.335876, -0.335876, \
--0.335876, 0.335876, 16.1859, 15.5141, 15.5141, 16.1859, -14.45, \
-14.45]
-YcondBluejay = [22.3025, 22.3025, 21.6308, 21.6308, 22.3025, 22.3025, 21.6308, \
-21.6308, 22.3025, 22.3025, 21.6308, 21.6308, 36.1, 36.1]
 
-#Posicoes X e Y de cada condutor individual (includindo pararaios) para o Rail normal. Os valores
-#foram calculados no arquivo do Mathematica fornecido pelo professor
-XcondRail = [6.33588, 5.66412, 5.66412, 6.33588, 11.3359, 10.6641, 10.6641, \
-11.3359, 15.3359, 14.6641, 14.6641, 15.3359, -6.33588, -5.66412, \
--5.66412, -6.33588, -11.3359, -10.6641, -10.6641, -11.3359, -15.3359, \
--14.6641, -14.6641, -15.3359, 8.8, -8.8]
-YcondRail = [23.5359, 23.5359, 22.8641, 22.8641, 33.5359, 33.5359, 32.8641, \
-32.8641, 23.5359, 23.5359, 22.8641, 22.8641, 23.5359, 23.5359, \
-22.8641, 22.8641, 33.5359, 33.5359, 32.8641, 32.8641, 23.5359, \
-23.5359, 22.8641, 22.8641, 42.7, 42.7]
-
-#plt.plot(XcondRail,YcondRail,'bx')
-#plt.show()
-#------------------------------------------------------------------------------------------------------
-
-#=================================================================================================
-#============================= Matrizes de Impedancia e Admitancia ===============================
-#=================================================================================================
-
-#Construcao da matriz de sequencias
-a = np.exp(1j * np.deg2rad(120))
-a2 = a**2
-A = np.array([[1, 1, 1], [1, a2, a], [1, a, a2]]) #matriz de sequencias
-
-npr = 2 #numero de pararaios
-
-
-#------------------------------------------ Bluejay -------------------------------------------
-tipo = "Bluejay"
-r_int = CondutoresEspecs[tipo][0]
-r_ext = CondutoresEspecs[tipo][1]
-nfase = len(XcondBluejay) - 2
-xc = np.array(XcondBluejay) #vetor com a posicao X de todos os condutores (fase e pararaio)
-yc = np.array(YcondBluejay) #vetor com a posicao Y de todos os condutores (fase e pararaio)
-rhoc = CondutoresEspecs[tipo][2]
-rhoc_pr = CondutoresEspecs["3/8 EHS"][2]
-rf = r_ext
-rpr = CondutoresEspecs["3/8 EHS"][1]
-
-#
-LinhaBluejay = Linha_transmissao(r_int, r_ext, nfase, npr, xc, yc, rhoc, rhoc_pr, r_ext, rpr)
-Z_bluejay = LinhaBluejay.impedancia()
-Y_bluejay = LinhaBluejay.admitancia()
-Z_bluejay = Z_bluejay.astype(np.csingle)
-Y_bluejay = Y_bluejay.astype(np.csingle)
-
-#Retiramos as info do pararaio das matrizes atraves da reducao de kron
-Zabc_bluejay = 0j + np.zeros((nfase,nfase))
-Yabc_bluejay = 0j + np.zeros((nfase,nfase))
-
-Zabc_bluejay = Z_bluejay[0:nfase,0:nfase] - Z_bluejay[0:nfase,nfase:] @ inv(Z_bluejay[nfase:,nfase:]) @ Z_bluejay[nfase:,0:nfase]
-Yabc_bluejay = Y_bluejay[0:nfase,0:nfase] - Y_bluejay[0:nfase,nfase:] @ inv(Y_bluejay[nfase:,nfase:]) @ Y_bluejay[nfase:,0:nfase]
-
-#z012_bluejay = inv(A)@Zabc_bluejay@A
-#y012_bluejay = inv(A)@Yabc_bluejay@A
-
-
-#------------------------------------- Rail normal ---------------------------------------
-
-#Atribuimos os valores que serão utilizados para chamar a classe. Raios, numero de condutores, posição espacial dos condutores, rhos
-tipo = "Rail Normal"
-r_int = CondutoresPos[tipo][0]
-r_int = CondutoresPos[tipo][1]
-nfase = len(XcondRail) - 2
-xc = np.array(XcondRail)
-yc = np.array(YcondRail)
-rhoc = CondutoresEspecs["Rail"][2]
-rhoc_pr = CondutoresEspecs["3/8 EHS"][2]
-rf = r_ext
-rpr = CondutoresEspecs["3/8 EHS"][1]
-
-LinhaRail = Linha_transmissao(r_int, r_ext, nfase, npr, xc, yc, rhoc, rhoc_pr, r_ext, rpr)
-
-#Uma vez construida a linha podemos determinar seus parametros de impedancia e admitancia
-Z_rail = LinhaRail.impedancia()
-Y_rail = LinhaRail.admitancia()
-Z_rail = Z_rail.astype(np.csingle) #Codigo necessario para o python ler o tipo de variavel 
-Y_rail = Y_rail.astype(np.csingle)
-#Redução de Kron para eliminar o para-raio
-Zabc_rail = 0j + np.zeros((6,6))
-Yabc_rail = 0j + np.zeros((6,6))
-Zabc_rail = Z_rail[0:nfase,0:nfase] - Z_rail[0:nfase,nfase:] @ inv(Z_rail[nfase:,nfase:]) @ Z_rail[nfase:,0:nfase]
-Yabc_rail = Y_rail[0:nfase,0:nfase] - Y_rail[0:nfase,nfase:] @ inv(Y_rail[nfase:,nfase:]) @ Y_rail[nfase:,0:nfase]
-
-#A = [[A, 0] matriz de sequencia precisa ser 6x6 agr
-#    [0, A]]
-zero = np.zeros((3,3)) #matriz de zeros auxiliar
-aux1 = np.concatenate((A,zero))
-aux2 = np.concatenate((zero,A))
-A = np.concatenate((aux1,aux2),axis=1) #matriz de sequencia para o caso 6x6
-
-
-#z012_rail = inv(A)@Zabc_rail@A
-#y012_rail = inv(A)@Yabc_rail@A
 
 
 #====================================================================================
@@ -181,9 +79,6 @@ for i in (CondutoresPos["Bluejay"][3]):
 for i in (CondutoresPos["Rail Normal"][3]):
     Yc.append(i)
     
-    
-print("Xc = %s" % Xc)
-print("Yc = %s" % Yc)
 
 #Para-raio
 Xpr=[]
@@ -201,8 +96,6 @@ Xpr.append(rail6-delta)
 for i in range(1,len(CondutoresPos["Rail Normal"][4]),2):
     Ypr.append(CondutoresPos["Rail Normal"][4][i]) 
 
-print("Xpr = %s" % Xpr)
-print("Ypr = %s" % Ypr)
 
 #Xc e Yc sao listas com as coordenadas dos centros dos condutores equivalentes das duas torres
 #Xpr e Ypr sao lista com as coordenadas dos centros dos pararaios dos das duas torres
@@ -210,5 +103,167 @@ print("Ypr = %s" % Ypr)
 
 #plt.plot(XcondRail,YcondRail,'bx',Xc[3:],Yc[3:],'ro')
 #plt.show()
+
+
+#=================================================================================================
+#============================= Matrizes de Impedancia e Admitancia ===============================
+#=================================================================================================
+
+#Construcao da matriz de sequencias
+a = np.exp(1j * np.deg2rad(120))
+a2 = a**2
+A = np.array([[1, 1, 1], [1, a2, a], [1, a, a2]]) #matriz de sequencias
+
+npr = 2 #numero de pararaios
+
+
+#------------------------------------------ Bluejay -------------------------------------------
+tipo = "Bluejay"
+r_ext = CondutoresEspecs[tipo][0]
+r_int = CondutoresEspecs[tipo][1]
+nfase = 3
+xc = np.array(Xc[0:3] + Xpr[0:2]) #vetor com a posicao X de todos os condutores do Bluejay(fase e pararaio)
+yc = np.array(Yc[0:3] + Ypr[0:2]) #vetor com a posicao Y de todos os condutores (fase e pararaio)
+rhoc = CondutoresEspecs[tipo][2]
+rhoc_pr = CondutoresEspecs["3/8 EHS"][2]
+rf = r_ext
+rpr = CondutoresEspecs["3/8 EHS"][1]
+
+#intanciacao do objeto que representa a linha bluejay
+LinhaBluejay = Linha_transmissao(r_int, r_ext, nfase, npr, xc, yc, rhoc, rhoc_pr, r_ext, rpr)
+Z_bluejay = LinhaBluejay.impedancia()
+Y_bluejay = LinhaBluejay.admitancia()
+Z_bluejay = Z_bluejay.astype(np.csingle)
+Y_bluejay = Y_bluejay.astype(np.csingle)
+
+#Retiramos as info do pararaio das matrizes atraves da reducao de kron
+Zabc_bluejay = 0j + np.zeros((nfase,nfase))
+Yabc_bluejay = 0j + np.zeros((nfase,nfase))
+
+Zabc_bluejay = Z_bluejay[0:nfase,0:nfase] - Z_bluejay[0:nfase,nfase:] @ inv(Z_bluejay[nfase:,nfase:]) @ Z_bluejay[nfase:,0:nfase]
+Yabc_bluejay = Y_bluejay[0:nfase,0:nfase] - Y_bluejay[0:nfase,nfase:] @ inv(Y_bluejay[nfase:,nfase:]) @ Y_bluejay[nfase:,0:nfase]
+
+z012_bluejay = inv(A)@Zabc_bluejay@A
+y012_bluejay = inv(A)@Yabc_bluejay@A
+
+#------------------------------------- Rail normal ---------------------------------------
+
+#Atribuimos os valores que serão utilizados para chamar a classe. Raios, numero de condutores, posição espacial dos condutores, rhos
+tipo = "Rail Normal"
+r_ext = CondutoresPos[tipo][0]
+r_int = CondutoresPos[tipo][1]
+nfase = 6
+xc = np.array(Xc[3:] + Xpr[2:]) #vetor com a posicao X de todos os condutores do Rail Normal(fase e pararaio)
+yc = np.array(Yc[3:] + Ypr[2:]) #vetor com a posicao Y de todos os condutores do Rail Normal (fase e pararaio)
+rhoc = CondutoresEspecs["Rail"][2]
+rhoc_pr = CondutoresEspecs["3/8 EHS"][2]
+rf = r_ext
+rpr = CondutoresEspecs["3/8 EHS"][1]
+
+#intanciacao do objeto que representa a linha rail normal
+LinhaRail = Linha_transmissao(r_int, r_ext, nfase, npr, xc, yc, rhoc, rhoc_pr, r_ext, rpr)
+
+#Uma vez construida a linha podemos determinar seus parametros de impedancia e admitancia
+Z_rail = LinhaRail.impedancia()
+Y_rail = LinhaRail.admitancia()
+Z_rail = Z_rail.astype(np.csingle) #Codigo necessario para o python ler o tipo de variavel 
+Y_rail = Y_rail.astype(np.csingle)
+#Redução de Kron para eliminar o para-raio
+Zabc_rail = 0j + np.zeros((6,6))
+Yabc_rail = 0j + np.zeros((6,6))
+Zabc_rail = Z_rail[0:nfase,0:nfase] - Z_rail[0:nfase,nfase:] @ inv(Z_rail[nfase:,nfase:]) @ Z_rail[nfase:,0:nfase]
+Yabc_rail = Y_rail[0:nfase,0:nfase] - Y_rail[0:nfase,nfase:] @ inv(Y_rail[nfase:,nfase:]) @ Y_rail[nfase:,0:nfase]
+
+#A = [[A, 0] matriz de sequencia precisa ser 6x6 agr
+#    [0, A]]
+zero = np.zeros((3,3)) #matriz de zeros auxiliar
+aux1 = np.concatenate((A,zero))
+aux2 = np.concatenate((zero,A))
+A = np.concatenate((aux1,aux2),axis=1) #matriz de sequencia para o caso 6x6
+
+
+z012_rail = inv(A)@Zabc_rail@A
+y012_rail = inv(A)@Yabc_rail@A
+
+
+#=================================================================================================
+#============================================= Quadripolos =======================================
+#=================================================================================================
+
+
+#======================================== Bluejay ==============================================
+#---------------- Gerador ---------------
+Sg = 300e6 #potencia
+Xg = 15.24 #impedancia [Ohms]
+
+Qg = np.array( [[1,Xg],[0, 1]] ) #quadripolo do gerador
+
+#------ Linha Bluejay (modelo pi) -------
+Z1 = z012_bluejay[1][1] #impedancia considerando a sequencia positiva
+Y1 = z012_bluejay[1][1] #admitancia considerando a sequencia negativa
+
+#Componentes da matriz de quadripolo
+A1 = 1 + Z1*Y1/2
+A2 = Z1
+A3 = Y1*(1 + Z1*Y1/4)
+A4 = 1 + Z1*Y1/2
+
+QL_b = np.array( [[A1, A2],[A3, A4]] ) #Quadripolo da linha bluejay
+
+
+#-------------- Testes --------------------------
+#fator de potencia unitario na carga
+
+Vg = 750e3 #tensao no gerador
+#Ig = Sg/(np.sqrt(3) * Vg) #corrente no gerador
+#VI_carga = inv(Qg @ QL_b) @ np.array([Vg, Ig])
+
+#Caso 1: Vazio sem compensacao -> Corrente na carga e nula, o segundda coluna do quadripolo e desconsiderada
+print("- Linha em vazio -")
+Vr = Vg/A1
+Ig = A3*Vr
+print("Vr = %.2e < %.2f   V =   %.2f pu" % (abs(Vr),np.rad2deg(np.angle(Vr)),abs(Vr/Vg)))
+print("Ig = %.2e < %.2f  A" % (abs(Ig),np.rad2deg(np.angle(Ig))))
+
+
+#Caso 2: Vazio com compensacao indutiva em derivacao na fonte e na carga
+Y = (1 - A1)/A2 #valor da admitancia de cada reator indutivo adicionado (p. 240 do Fuchs Vol. 1)
+
+A1c = A1 + A2*Y
+A2c = A2
+A3c = A3 + A1*Y + A4*Y + A2*Y*Y
+A4c = A4 + A2*Y
+
+Qcomp = np.array( [[A1c, A2c],[A3c, A4c]] ) #Quadripolo equivalente considerando a compensacao
+
+print("\n\n- Linha com compensacao -")
+Vr = Vg/A1c
+Ig = A3c*Vr
+print("Vr = %.2e < %.2f   V =   %.2f pu" % (abs(Vr),np.rad2deg(np.angle(Vr)),abs(Vr/Vg)))
+print("Ig = %.2e < %.2f  A" % (abs(Ig),np.rad2deg(np.angle(Ig))))
+
+
+
+
+
+
+#======================================== Rail Normal ===========================================
+
+#------------- Gerador --------------
+Sg = 450e6 #potencia [VA]
+Xg = 15.24 #impedancia [Ohms]
+
+Qg = np.array( [[1,Xg],[0, 1]] )
+
+#------ Linha Bluejay (modelo pi) -------
+Z1 = z012_rail[1][1] #impedancia considerando a sequencia positiva
+Y1 = z012_rail[1][1] #admitancia considerando a sequencia negativa
+
+A1 = 1 + Z1*Y1/2
+A2 = Z1
+A3 = Y1*(1 + Z1*Y1/4)
+A4 = 1 + Z1*Y1/2
+
+QL_r = np.array( [[A1, A2],[A3, A4]] ) #quadripolo da linha rail normal
 
 
